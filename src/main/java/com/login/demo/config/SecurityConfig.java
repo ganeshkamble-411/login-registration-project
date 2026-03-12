18032cbabf83a9764ad7135cb0e1baff74712977
ganeshkamble-411 <gk6569471@gmail.com>
2026-03-12 23:17:30 +0530

    Added LoginPage Frontend

--- src/main/java/com/login/demo/config/SecurityConfig.java
+++ src/main/java/com/login/demo/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import java.util.List;
 
+
 @Configuration
 public class SecurityConfig {
 
